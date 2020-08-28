9bc334ce6d18233d755357ffafa214eff4b9fa46
jf-tech <jf.tech.llc@gmail.com>
2020-08-28 13:00:23 +1200

    Remove node package for now and add jsonutil
    - Remove node package for now as we'll transition to using xmlquery package directly.
    - add jsonutil for pretty json marshaling helpers

--- jsonutil/jsonutil.go
+++ jsonutil/jsonutil.go
@@ -9,6 +9,7 @@ const (
 	prettyIndent = "\t"
 )
 
+// PrettyMarshal does a JSON marshaling of 'v' with human readable output.
 func PrettyMarshal(v interface{}) (string, error) {
 	valueBuf := new(bytes.Buffer)
 	enc := json.NewEncoder(valueBuf)
@@ -21,6 +22,9 @@ func PrettyMarshal(v interface{}) (string, error) {
 	return valueBuf.String(), nil
 }
 
+// BestEffortPrettyMarshal does a best effort JSON marshaling of 'v' with human
+// readable output. '{}' will be produced if there is any JSON marshal error. This
+// function never fails.
 func BestEffortPrettyMarshal(v interface{}) string {
 	jsonStr, err := PrettyMarshal(v)
 	if err != nil {
@@ -28,3 +32,6 @@ func BestEffortPrettyMarshal(v interface{}) string {
 	}
 	return jsonStr
 }
+
+// BPM is a shortcut (mostly used ing tests) to BestEffortPrettyMarshal.
+var BPM = BestEffortPrettyMarshal
