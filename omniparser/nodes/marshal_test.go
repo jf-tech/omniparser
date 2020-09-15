d7c53d613408eb9c2bf6e96e5e534bb31f2a0bb8
JF Technology <57098161+jf-tech@users.noreply.github.com>
2020-09-15 13:59:22 +1200

    Update nodes.JSONify so that it can marshal a *node.Node into a JSON without those pointers. (#40)
    This is precursor for us be able to pass the current contextual *node.Node to a javascript.....yes, javascript
    engine integration is coming....

--- omniparser/nodes/marshal_test.go
+++ omniparser/nodes/marshal_test.go
@@ -7,25 +7,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNodeName(t *testing.T) {
+func TestNodePtrName(t *testing.T) {
 	for _, test := range []struct {
 		name     string
 		n        *node.Node
 		expected string
 	}{
 		{name: "nil", n: nil, expected: "(nil)"},
-		{name: "root", n: &node.Node{Type: node.DocumentNode}, expected: "(ROOT)"},
-		{name: "decl", n: &node.Node{Type: node.DeclarationNode, Data: "xml"}, expected: "(DECL xml)"},
-		{name: "elem w/o ns", n: &node.Node{Type: node.ElementNode, Data: "A"}, expected: "(ELEM A)"},
-		{name: "elem w/ ns", n: &node.Node{Type: node.ElementNode, Data: "A", Prefix: "ns"}, expected: "(ELEM ns:A)"},
-		{name: "text", n: &node.Node{Type: node.TextNode, Data: "data"}, expected: "(TEXT 'data')"},
-		{name: "cdata", n: &node.Node{Type: node.CharDataNode, Data: "data"}, expected: "(CDATA 'data')"},
-		{name: "comment", n: &node.Node{Type: node.CommentNode, Data: "huh"}, expected: "(COMMENT 'huh')"},
-		{name: "attr", n: &node.Node{Type: node.AttributeNode, Data: "attr"}, expected: "(ATTR attr)"},
+		{name: "root", n: &node.Node{Type: node.DocumentNode}, expected: "(DocumentNode)"},
+		{name: "decl", n: &node.Node{Type: node.DeclarationNode, Data: "xml"}, expected: "(DeclarationNode xml)"},
+		{name: "elem w/o ns", n: &node.Node{Type: node.ElementNode, Data: "A"}, expected: "(ElementNode A)"},
+		{name: "elem w/ ns", n: &node.Node{Type: node.ElementNode, Data: "A", Prefix: "ns"}, expected: "(ElementNode ns:A)"},
+		{name: "text", n: &node.Node{Type: node.TextNode, Data: "data"}, expected: "(TextNode 'data')"},
+		{name: "cdata", n: &node.Node{Type: node.CharDataNode, Data: "data"}, expected: "(CharDataNode 'data')"},
+		{name: "comment", n: &node.Node{Type: node.CommentNode, Data: "huh"}, expected: "(CommentNode 'huh')"},
+		{name: "attr", n: &node.Node{Type: node.AttributeNode, Data: "attr"}, expected: "(AttributeNode attr)"},
 		{name: "unknown", n: &node.Node{Type: node.NodeType(99999), Data: "what"}, expected: "(unknown 'what')"},
 	} {
 		t.Run(test.name, func(t *testing.T) {
-			assert.Equal(t, test.expected, nodeName(test.n))
+			assert.Equal(t, test.expected, nodePtrName(test.n))
 		})
 	}
 }
