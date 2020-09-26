d3f172ee1000d5b7abc1e3c72ff9128a4b9d6af4
jf-tech <jf.tech.llc@gmail.com>
2020-09-27 10:22:19 +1300

    To fix heroku dockerfile building time lack of git access, switch to use heroku lab dyno-metadata to obtain git info at runtime

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM golang:1.14
 WORKDIR /omniparser
 COPY . .
-RUN go build -ldflags "-X main.gitCommit=$(git rev-list -1 HEAD) -X main.buildEpochSec=$(date +%s)" -o cli/op cli/op.go
+# when Dockerfile building in heroku, .git isn't available (removed by heroku slug compiler)
+# so git command will fail. in this case, just put "" empty string into the var, we'll figure
+# out the commit another way in code.
+RUN go build -ldflags "-X main.gitCommit=$(git rev-parse HEAD 2>/dev/null || echo) -X main.buildEpochSec=$(date +%s)" -o cli/op cli/op.go
 RUN cli/op --help
 CMD cli/op server
--- cli/op.go
+++ cli/op.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	"os"
-	"strconv"
-	"time"
+	"strings"
 
 	"github.com/jf-tech/omniparser/cli/cmd"
 )
 
 var (
 	// To populate these vars from build/run
-	//   go build/run -ldflags "-X main.gitCommit=$(git rev-list -1 HEAD) -X main.buildEpochSec=$(date +%s)" ...
+	//   go build/run -ldflags "-X main.gitCommit=$(git rev-parse HEAD) -X main.buildEpochSec=$(date +%s)" ...
 	gitCommit     string
 	buildEpochSec string
 )
 
 func main() {
-	switch gitCommit {
-	case "":
-		gitCommit = "(unknown commit)"
-	default:
-		gitCommit = string(([]rune(gitCommit))[:7])
-	}
-	if buildEpochSec == "" {
-		buildEpochSec = strconv.FormatInt(time.Now().Unix(), 10)
-	}
-	if err := cmd.Execute(gitCommit, buildEpochSec); err != nil {
+	if err := cmd.Execute(getGitCommit(), getBuildEpochSec()); err != nil {
 		os.Exit(1)
 	}
+}
+
+func getGitCommit() string {
+	shaPrefix := func(sha string) string {
+		return string(([]rune(sha))[:7])
+	}
+	if gitCommit != "" {
+		gitCommit = shaPrefix(gitCommit)
+		return gitCommit
+	}
+	// https://devcenter.heroku.com/articles/dyno-metadata
+	gitCommit = os.Getenv("HEROKU_SLUG_COMMIT")
+	if gitCommit != "" {
+		gitCommit = shaPrefix(gitCommit)
+		return gitCommit
+	}
+	// but sometimes, HEROKU_SLUG_COMMIT is might be empty, let's
+	// try HEROKU_SLUG_DESCRIPTION, which seems more reliable.
+	gitCommit = os.Getenv("HEROKU_SLUG_DESCRIPTION")
+	// although HEROKU_SLUG_DESCRIPTION is of "Deploy <sha>" format
+	if strings.HasPrefix(gitCommit, "Deploy ") {
+		gitCommit = shaPrefix(gitCommit[len("Deploy "):])
+		return gitCommit
+	}
+	gitCommit = "(unknown)"
+	return gitCommit
+}
+
+func getBuildEpochSec() string {
+	if buildEpochSec != "" {
+		return buildEpochSec
+	}
+	buildEpochSec = "(unknown)"
+	return buildEpochSec
 }
