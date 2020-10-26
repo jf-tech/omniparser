961247ea799cb1d0bf61bd608f23a01db841e61f
JF Technology <57098161+jf-tech@users.noreply.github.com>
2020-10-27 08:10:14 +1300

    Adding EDI `file_declaration` json schema
    The diffs from previous generation:
    - `fixed_length_in_bytes` is gone: in practice, never used it once. Why bother adding it here.
    - `release_character`'s `maxLength: 1` is gone: now our library code is more capable (and surprisingly much faster) to deal with esc whose len>1.

--- extensions/omniv21/validation/jsonvalidate.go
+++ extensions/omniv21/validation/jsonvalidate.go
@@ -2,3 +2,4 @@ package validation
 
 //go:generate sh -c "go run ../../../validation/gen/gen.go -json transform_declarations.json -varname JSONSchemaTransformDeclarations > ./transformDeclarations.go"
 //go:generate sh -c "go run ../../../validation/gen/gen.go -json csv_file_declaration.json -varname JSONSchemaCSVFileDeclaration > ./csvFileDeclaration.go"
+//go:generate sh -c "go run ../../../validation/gen/gen.go -json edi_file_declaration.json -varname JSONSchemaEDIFileDeclaration > ./ediFileDeclaration.go"
