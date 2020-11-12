6bcd509372c76d7d0cb5f28d2ba4a886e008f60a
JF Technology <57098161+jf-tech@users.noreply.github.com>
2020-11-13 06:55:13 +1300

    fixed-length fileformat schema validation (#112)
    fixed-length fileformat schema validation

---
+++ extensions/omniv21/fileformat/fixedlength/format.go
@@ -0,0 +1,139 @@
+package fixedlength
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+
+	"github.com/jf-tech/go-corelib/caches"
+	"github.com/jf-tech/go-corelib/strs"
+
+	"github.com/jf-tech/omniparser/errs"
+	"github.com/jf-tech/omniparser/extensions/omniv21/fileformat"
+	"github.com/jf-tech/omniparser/extensions/omniv21/transform"
+	v21validation "github.com/jf-tech/omniparser/extensions/omniv21/validation"
+	"github.com/jf-tech/omniparser/validation"
+)
+
+const (
+	fileFormatFixedLength = "fixed-length"
+)
+
+type fixedLengthFileFormat struct {
+	schemaName               string
+	autoGenEnvelopeNameIndex int
+}
+
+// NewFixedLengthFileFormat creates a FileFormat for fixed-length files.
+func NewFixedLengthFileFormat(schemaName string) fileformat.FileFormat {
+	return &fixedLengthFileFormat{schemaName: schemaName}
+}
+
+type fixedLengthFormatRuntime struct {
+	Decl  *fileDecl `json:"file_declaration"`
+	XPath string
+}
+
+func (f *fixedLengthFileFormat) ValidateSchema(
+	format string, schemaContent []byte, finalOutputDecl *transform.Decl) (interface{}, error) {
+	if format != fileFormatFixedLength {
+		return nil, errs.ErrSchemaNotSupported
+	}
+	err := validation.SchemaValidate(f.schemaName, schemaContent, v21validation.JSONSchemaFixedLengthFileDeclaration)
+	if err != nil {
+		// err is already context formatted.
+		return nil, err
+	}
+	var runtime fixedLengthFormatRuntime
+	_ = json.Unmarshal(schemaContent, &runtime) // JSON schema validation earlier guarantees Unmarshal success.
+	err = f.validateFileDecl(runtime.Decl)
+	if err != nil {
+		// err is already context formatted.
+		return nil, err
+	}
+	if finalOutputDecl == nil {
+		return nil, f.FmtErr("'FINAL_OUTPUT' is missing")
+	}
+	runtime.XPath = strings.TrimSpace(strs.StrPtrOrElse(finalOutputDecl.XPath, ""))
+	if runtime.XPath != "" {
+		_, err := caches.GetXPathExpr(runtime.XPath)
+		if err != nil {
+			return nil, f.FmtErr("'FINAL_OUTPUT.xpath' (value: '%s') is invalid, err: %s",
+				runtime.XPath, err.Error())
+		}
+	}
+	return &runtime, nil
+}
+
+func (f *fixedLengthFileFormat) validateFileDecl(decl *fileDecl) error {
+	targetSeen := false
+	namesSeen := map[string]bool{}
+	for _, envelope := range decl.Envelopes {
+		if targetSeen && !envelope.NotTarget {
+			return f.FmtErr("cannot have more than one target envelope")
+		}
+		targetSeen = targetSeen || !envelope.NotTarget
+		if envelope.Name == nil {
+			f.autoGenEnvelopeNameIndex++
+			envelope.Name = strs.StrPtr(strconv.Itoa(f.autoGenEnvelopeNameIndex))
+		}
+		if _, found := namesSeen[*envelope.Name]; found {
+			return f.FmtErr("more than one envelope has the name '%s'", *envelope.Name)
+		}
+		namesSeen[*envelope.Name] = true
+		if err := f.validateByHeaderFooter(envelope.ByHeaderFooter); err != nil {
+			return err
+		}
+		if err := f.validateColumns(envelope.Columns); err != nil {
+			return err
+		}
+	}
+	if !targetSeen {
+		return f.FmtErr("missing target envelope")
+	}
+	return nil
+}
+
+func (f *fixedLengthFileFormat) validateByHeaderFooter(decl *byHeaderFooterDecl) error {
+	if decl == nil {
+		return nil
+	}
+	_, err := caches.GetRegex(decl.Header)
+	if err != nil {
+		return f.FmtErr("invalid 'header' regex '%s': %s", decl.Header, err.Error())
+	}
+	_, err = caches.GetRegex(decl.Footer)
+	if err != nil {
+		return f.FmtErr("invalid 'footer' regex '%s': %s", decl.Footer, err.Error())
+	}
+	return nil
+}
+
+func (f *fixedLengthFileFormat) validateColumns(cols []*columnDecl) error {
+	columnNamesSeen := map[string]bool{}
+	for _, col := range cols {
+		if _, found := columnNamesSeen[col.Name]; found {
+			return f.FmtErr("more than one column has the name '%s'", col.Name)
+		}
+		columnNamesSeen[col.Name] = true
+		if col.LinePattern != nil {
+			if _, err := caches.GetRegex(*col.LinePattern); err != nil {
+				return f.FmtErr("invalid 'line_pattern' regex '%s': %s", *col.LinePattern, err.Error())
+			}
+		}
+	}
+	return nil
+}
+
+func (f *fixedLengthFileFormat) CreateFormatReader(
+	name string, r io.Reader, runtime interface{}) (fileformat.FormatReader, error) {
+	// TODO
+	_ = runtime.(*fixedLengthFormatRuntime)
+	return nil, nil
+}
+
+func (f *fixedLengthFileFormat) FmtErr(format string, args ...interface{}) error {
+	return fmt.Errorf("schema '%s': %s", f.schemaName, fmt.Sprintf(format, args...))
+}
--- extensions/omniv21/validation/jsonvalidate.go
+++ extensions/omniv21/validation/jsonvalidate.go
@@ -1,5 +1,6 @@
 package validation
 
-//go:generate sh -c "go run ../../../validation/gen/gen.go -json transform_declarations.json -varname JSONSchemaTransformDeclarations > ./transformDeclarations.go"
-//go:generate sh -c "go run ../../../validation/gen/gen.go -json csv_file_declaration.json -varname JSONSchemaCSVFileDeclaration > ./csvFileDeclaration.go"
-//go:generate sh -c "go run ../../../validation/gen/gen.go -json edi_file_declaration.json -varname JSONSchemaEDIFileDeclaration > ./ediFileDeclaration.go"
+//go:generate sh -c "go run ../../../validation/gen/gen.go -json transformDeclarations.json -varname JSONSchemaTransformDeclarations > ./transformDeclarations.go"
+//go:generate sh -c "go run ../../../validation/gen/gen.go -json csvFileDeclaration.json -varname JSONSchemaCSVFileDeclaration > ./csvFileDeclaration.go"
+//go:generate sh -c "go run ../../../validation/gen/gen.go -json ediFileDeclaration.json -varname JSONSchemaEDIFileDeclaration > ./ediFileDeclaration.go"
+//go:generate sh -c "go run ../../../validation/gen/gen.go -json fixedlengthFileDeclaration.json -varname JSONSchemaFixedLengthFileDeclaration > ./fixedlengthFileDeclaration.go"
