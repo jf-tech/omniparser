f37c4f4c2b69a8827a38009cd7b055cd53b9f032
JF Technology <57098161+jf-tech@users.noreply.github.com>
2020-11-15 20:32:48 +1300

    add reader.readByHeaderFooterEnvelope

--- extensions/omniv21/fileformat/fixedlength/reader_test.go
+++ extensions/omniv21/fileformat/fixedlength/reader_test.go
@@ -72,8 +72,7 @@ func TestReadLine(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, "read error", err.Error())
 	assert.Nil(t, line)
-	// reading error (unless it's EOF) bumps current line number
-	assert.Equal(t, 2, r.line)
+	assert.Equal(t, 1, r.line)
 }
 
 func TestReadByRowsEnvelope_ByRowsDefault(t *testing.T) {
@@ -133,6 +132,7 @@ func TestReadByRowsEnvelope_ByRowsNonDefault(t *testing.T) {
 
 	n, err = r.readByRowsEnvelope()
 	assert.Error(t, err)
+	assert.True(t, IsErrInvalidEnvelope(err))
 	assert.Equal(t, "input 'test' line 6: incomplete envelope, missing 2 row(s)", err.Error())
 	assert.Nil(t, n)
 }
@@ -161,6 +161,159 @@ func BenchmarkReadByRowsEnvelope(b *testing.B) {
 		r := testReader(strings.NewReader(benchReadByRowsEnvelopeInput), benchReadByRowsEnvelopeDecl)
 		for {
 			n, err := r.readByRowsEnvelope()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				b.FailNow()
+			}
+			idr.RemoveAndReleaseTree(n)
+		}
+	}
+}
+
+func TestReadByHeaderFooterEnvelope_EOFBeforeStart(t *testing.T) {
+	r := testReader(strings.NewReader(""), &fileDecl{Envelopes: []*envelopeDecl{{Name: strs.StrPtr("env1")}}})
+	n, err := r.readByHeaderFooterEnvelope()
+	assert.Equal(t, io.EOF, err)
+	assert.Nil(t, n)
+}
+
+func TestReadByHeaderFooterEnvelope_ReadErrorBeforeStart(t *testing.T) {
+	r := testReader(
+		testlib.NewMockReadCloser("read error", nil),
+		&fileDecl{Envelopes: []*envelopeDecl{{Name: strs.StrPtr("env1")}}})
+	n, err := r.readByHeaderFooterEnvelope()
+	assert.Error(t, err)
+	assert.True(t, IsErrInvalidEnvelope(err))
+	assert.Equal(t, `input 'test' line 1: incomplete envelope: read error`, err.Error())
+	assert.Nil(t, n)
+}
+
+func TestReadByHeaderFooterEnvelope_NoEnvelopeMatch(t *testing.T) {
+	r := testReader(
+		strings.NewReader("efg"),
+		&fileDecl{Envelopes: []*envelopeDecl{{
+			Name:           strs.StrPtr("env1"),
+			ByHeaderFooter: &byHeaderFooterDecl{Header: "abc", Footer: "abc"},
+		}}})
+	n, err := r.readByHeaderFooterEnvelope()
+	assert.Equal(t, io.EOF, err)
+	assert.Nil(t, n)
+}
+
+func TestReadByHeaderFooterEnvelope_IncompleteEnvelope(t *testing.T) {
+	r := testReader(
+		strings.NewReader("abc"),
+		&fileDecl{Envelopes: []*envelopeDecl{{
+			Name:           strs.StrPtr("env1"),
+			ByHeaderFooter: &byHeaderFooterDecl{Header: "abc", Footer: "efg"},
+		}}})
+	n, err := r.readByHeaderFooterEnvelope()
+	assert.Error(t, err)
+	assert.True(t, IsErrInvalidEnvelope(err))
+	assert.Equal(t, `input 'test' line 2: incomplete envelope: EOF`, err.Error())
+	assert.Nil(t, n)
+}
+
+func lf(s string) string { return s + "\n" }
+
+func TestReadByHeaderFooterEnvelope_Success1(t *testing.T) {
+	r := testReader(
+		strings.NewReader(
+			lf("begin")+
+				lf("header-01")+
+				lf("a001-abc")+
+				lf("a002-def")+
+				lf("a003-ghi")+
+				lf("footer")+
+				lf("header-02")+
+				lf("a001-012")+
+				lf("a002-345")+
+				lf("a003-678")+
+				lf("footer")+
+				lf("end")),
+		&fileDecl{Envelopes: []*envelopeDecl{
+			{
+				Name:           strs.StrPtr("begin"),
+				ByHeaderFooter: &byHeaderFooterDecl{Header: "^begin", Footer: "^begin"},
+			},
+			{
+				Name:           strs.StrPtr("data"),
+				ByHeaderFooter: &byHeaderFooterDecl{Header: "^header", Footer: "^footer"},
+				Columns: []*columnDecl{
+					{Name: "data_id", StartPos: 8, Length: 2, LinePattern: strs.StrPtr("^header")},
+					{Name: "a001_first2chars", StartPos: 6, Length: 2, LinePattern: strs.StrPtr("^a001")},
+					{Name: "a003_last2chars", StartPos: 7, Length: 2, LinePattern: strs.StrPtr("^a003")},
+					{Name: "a001_last1char", StartPos: 8, Length: 1, LinePattern: strs.StrPtr("^a001")},
+				},
+			},
+			{
+				Name:           strs.StrPtr("end"),
+				ByHeaderFooter: &byHeaderFooterDecl{Header: "^end", Footer: "^end"},
+			},
+		}})
+	n, err := r.readByHeaderFooterEnvelope()
+	assert.NoError(t, err)
+	assert.Equal(t, "begin", n.Data)
+
+	n, err = r.readByHeaderFooterEnvelope()
+	assert.NoError(t, err)
+	assert.Equal(t,
+		`{"a001_first2chars":"ab","a001_last1char":"c","a003_last2chars":"hi","data_id":"01"}`, idr.JSONify2(n))
+
+	n, err = r.readByHeaderFooterEnvelope()
+	assert.NoError(t, err)
+	assert.Equal(t,
+		`{"a001_first2chars":"01","a001_last1char":"2","a003_last2chars":"78","data_id":"02"}`, idr.JSONify2(n))
+
+	n, err = r.readByHeaderFooterEnvelope()
+	assert.NoError(t, err)
+	assert.Equal(t, "end", n.Data)
+
+	n, err = r.readByHeaderFooterEnvelope()
+	assert.Equal(t, io.EOF, err)
+	assert.Nil(t, n)
+}
+
+var (
+	benchReadByHeaderFooterEnvelopeInput = lf("begin") +
+		strings.Repeat(
+			lf("header")+
+				lf("a001-abcdefghijklmnopqrstuvwxyz0123456789")+
+				lf("a002-abcdefghijklmnopqrstuvwxyz0123456789")+
+				lf("a003-abcdefghijklmnopqrstuvwxyz0123456789")+
+				lf("footer"), 1000) +
+		lf("end")
+	benchReadByHeaderFooterEnvelopeDecl = &fileDecl{
+		Envelopes: []*envelopeDecl{
+			{
+				Name:           strs.StrPtr("begin"),
+				ByHeaderFooter: &byHeaderFooterDecl{Header: "^begin", Footer: "^begin"},
+			},
+			{
+				Name:           strs.StrPtr("data"),
+				ByHeaderFooter: &byHeaderFooterDecl{Header: "^header", Footer: "^footer"},
+				Columns: []*columnDecl{
+					{Name: "a001_1", StartPos: 6, Length: 12, LinePattern: strs.StrPtr("^a001")},
+					{Name: "a003_1", StartPos: 7, Length: 9, LinePattern: strs.StrPtr("^a003")},
+					{Name: "a001_2", StartPos: 30, Length: 20, LinePattern: strs.StrPtr("^a001")},
+				},
+			},
+			{
+				Name:           strs.StrPtr("end"),
+				ByHeaderFooter: &byHeaderFooterDecl{Header: "^end", Footer: "^end"},
+			},
+		},
+	}
+)
+
+// BenchmarkReadByHeaderFooterEnvelope-8   	     310	   3819649 ns/op	  213840 B/op	   14009 allocs/op
+func BenchmarkReadByHeaderFooterEnvelope(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		r := testReader(strings.NewReader(benchReadByHeaderFooterEnvelopeInput), benchReadByHeaderFooterEnvelopeDecl)
+		for {
+			n, err := r.readByHeaderFooterEnvelope()
 			if err != nil {
 				if err == io.EOF {
 					break
