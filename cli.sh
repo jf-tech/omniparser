f057be36ba3f67824d54e29ac9a2dd0bedbd50af
JF Technology <57098161+jf-tech@users.noreply.github.com>
2020-11-19 13:26:03 +1300

    getting started (#121)

--- cli.sh
+++ cli.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR=$(pwd `dirname "$0"`)
-go run -ldflags "-X main.gitCommit=$(git rev-parse HEAD) -X main.buildEpochSec=$(date +%s)" $SCRIPT_DIR/cli/op.go "$@"
+CUR_DIR=$(pwd)
+SCRIPT_DIR=$(dirname "$0")
+cd $SCRIPT_DIR && \
+  go build \
+    -o $CUR_DIR/op \
+    -ldflags "-X main.gitCommit=$(git rev-parse HEAD) -X main.buildEpochSec=$(date +%s)" \
+    $SCRIPT_DIR/cli/op.go
+cd $CUR_DIR && ./op "$@"
---
+++ doc/customfuncs.md
@@ -0,0 +1,1 @@
+# Custom Function Reference
