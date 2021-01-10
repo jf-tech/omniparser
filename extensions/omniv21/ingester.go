2a64f8ea7f04affad56bd316d409fcb21a969ec4
JF Technology <57098161+jf-tech@users.noreply.github.com>
2021-01-11 08:28:43 +1300

    RawRecord PR follow-up - address DGollings' comment

--- extensions/omniv21/ingester.go
+++ extensions/omniv21/ingester.go
@@ -9,19 +9,21 @@ import (
 	"github.com/jf-tech/omniparser/extensions/omniv21/fileformat"
 	"github.com/jf-tech/omniparser/extensions/omniv21/transform"
 	"github.com/jf-tech/omniparser/idr"
+	"github.com/jf-tech/omniparser/schemahandler"
 	"github.com/jf-tech/omniparser/transformctx"
 )
 
-// RawRecord contains the raw data ingested in from the input stream in the form of an IDR tree.
-// Note callers outside this package should absolutely make **NO** modifications to the content of
-// RawRecord. Treat it like read-only.
-type RawRecord struct {
-	Node *idr.Node
+type rawRecord struct {
+	node *idr.Node
 }
 
-// UUIDv3 returns a stable MD5(v3) hash of the RawRecord.
-func (rr *RawRecord) UUIDv3() string {
-	hash, _ := customfuncs.UUIDv3(nil, idr.JSONify2(rr.Node))
+func (rr *rawRecord) Raw() interface{} {
+	return rr.node
+}
+
+// Checksum returns a stable MD5(v3) hash of the rawRecord.
+func (rr *rawRecord) Checksum() string {
+	hash, _ := customfuncs.UUIDv3(nil, idr.JSONify2(rr.node))
 	return hash
 }
 
@@ -31,19 +33,19 @@ type ingester struct {
 	customParseFuncs transform.CustomParseFuncs // Deprecated.
 	ctx              *transformctx.Ctx
 	reader           fileformat.FormatReader
-	rawRecord        RawRecord
+	rawRecord        rawRecord
 }
 
 // Read ingests a raw record from the input stream, transforms it according the given schema and return
 // the raw record, transformed JSON bytes.
-func (g *ingester) Read() (interface{}, []byte, error) {
-	if g.rawRecord.Node != nil {
-		g.reader.Release(g.rawRecord.Node)
-		g.rawRecord.Node = nil
+func (g *ingester) Read() (schemahandler.RawRecord, []byte, error) {
+	if g.rawRecord.node != nil {
+		g.reader.Release(g.rawRecord.node)
+		g.rawRecord.node = nil
 	}
 	n, err := g.reader.Read()
 	if n != nil {
-		g.rawRecord.Node = n
+		g.rawRecord.node = n
 	}
 	if err != nil {
 		// Read() supposed to have already done CtxAwareErr error wrapping. So directly return.
--- transform_test.go
+++ transform_test.go
@@ -9,11 +9,26 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/jf-tech/omniparser/errs"
+	"github.com/jf-tech/omniparser/schemahandler"
 )
 
 type testReadCall struct {
-	record []byte
+	result []byte
 	err    error
+}
+
+func (trc testReadCall) Checksum() string {
+	if trc.err != nil {
+		panic("Checksum() called when err != nil")
+	}
+	return fmt.Sprintf("checksum of raw record of '%s'", string(trc.result))
+}
+
+func (trc testReadCall) Raw() interface{} {
+	if trc.err != nil {
+		panic("Raw() called when err != nil")
+	}
+	return fmt.Sprintf("raw record of '%s'", string(trc.result))
 }
 
 type testIngester struct {
@@ -22,13 +37,13 @@ type testIngester struct {
 	continuableErrs map[error]bool
 }
 
-func (g *testIngester) Read() (interface{}, []byte, error) {
+func (g *testIngester) Read() (schemahandler.RawRecord, []byte, error) {
 	if g.readCalled >= len(g.readCalls) {
 		panic(fmt.Sprintf("Read() called %d time(s), but not enough mock entries setup", g.readCalled))
 	}
 	r := g.readCalls[g.readCalled]
 	g.readCalled++
-	return fmt.Sprintf("raw record %d", g.readCalled-1), r.record, r.err
+	return r, r.result, r.err
 }
 
 func (g *testIngester) IsContinuableError(err error) bool {
@@ -45,9 +60,9 @@ func TestTransform_Read_EndWithEOF(t *testing.T) {
 	tfm := &transform{
 		ingester: &testIngester{
 			readCalls: []testReadCall{
-				{record: []byte("1st good read")},
+				{result: []byte("1st good read")},
 				{err: continuableErr1},
-				{record: []byte("2nd good read")},
+				{result: []byte("2nd good read")},
 				{err: io.EOF},
 			},
 			continuableErrs: map[error]bool{continuableErr1: true},
@@ -56,16 +71,17 @@ func TestTransform_Read_EndWithEOF(t *testing.T) {
 	record, err := tfm.Read()
 	assert.NoError(t, err)
 	assert.Equal(t, "1st good read", string(record))
-	raw, err := tfm.CurrentRawRecord()
+	raw, err := tfm.RawRecord()
 	assert.NoError(t, err)
-	assert.Equal(t, "raw record 0", raw.(string))
+	assert.Equal(t, "raw record of '1st good read'", raw.Raw())
+	assert.Equal(t, "checksum of raw record of '1st good read'", raw.Checksum())
 
 	record, err = tfm.Read()
 	assert.Error(t, err)
 	assert.True(t, errs.IsErrTransformFailed(err))
 	assert.Equal(t, continuableErr1.Error(), err.Error())
 	assert.Nil(t, record)
-	raw, err = tfm.CurrentRawRecord()
+	raw, err = tfm.RawRecord()
 	assert.Error(t, err)
 	assert.True(t, errs.IsErrTransformFailed(err))
 	assert.Nil(t, raw)
@@ -73,15 +89,16 @@ func TestTransform_Read_EndWithEOF(t *testing.T) {
 	record, err = tfm.Read()
 	assert.NoError(t, err)
 	assert.Equal(t, "2nd good read", string(record))
-	raw, err = tfm.CurrentRawRecord()
+	raw, err = tfm.RawRecord()
 	assert.NoError(t, err)
-	assert.Equal(t, "raw record 2", raw.(string))
+	assert.Equal(t, "raw record of '2nd good read'", raw.Raw())
+	assert.Equal(t, "checksum of raw record of '2nd good read'", raw.Checksum())
 
 	record, err = tfm.Read()
 	assert.Error(t, err)
 	assert.Equal(t, io.EOF, err)
 	assert.Nil(t, record)
-	raw, err = tfm.CurrentRawRecord()
+	raw, err = tfm.RawRecord()
 	assert.Error(t, err)
 	assert.Equal(t, io.EOF, err)
 	assert.Nil(t, raw)
@@ -91,7 +108,7 @@ func TestTransform_Read_EndWithEOF(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, io.EOF, err)
 	assert.Nil(t, record)
-	raw, err = tfm.CurrentRawRecord()
+	raw, err = tfm.RawRecord()
 	assert.Error(t, err)
 	assert.Equal(t, io.EOF, err)
 	assert.Nil(t, raw)
@@ -101,7 +118,7 @@ func TestTransform_Read_EndWithNonContinuableError(t *testing.T) {
 	tfm := &transform{
 		ingester: &testIngester{
 			readCalls: []testReadCall{
-				{record: []byte("1st good read")},
+				{result: []byte("1st good read")},
 				{err: errors.New("fatal error")},
 			},
 		},
@@ -109,16 +126,17 @@ func TestTransform_Read_EndWithNonContinuableError(t *testing.T) {
 	record, err := tfm.Read()
 	assert.NoError(t, err)
 	assert.Equal(t, "1st good read", string(record))
-	raw, err := tfm.CurrentRawRecord()
+	raw, err := tfm.RawRecord()
 	assert.NoError(t, err)
-	assert.Equal(t, "raw record 0", raw.(string))
+	assert.Equal(t, "raw record of '1st good read'", raw.Raw())
+	assert.Equal(t, "checksum of raw record of '1st good read'", raw.Checksum())
 
 	record, err = tfm.Read()
 	assert.Error(t, err)
 	assert.False(t, errs.IsErrTransformFailed(err))
 	assert.Equal(t, "fatal error", err.Error())
 	assert.Nil(t, record)
-	raw, err = tfm.CurrentRawRecord()
+	raw, err = tfm.RawRecord()
 	assert.Error(t, err)
 	assert.False(t, errs.IsErrTransformFailed(err))
 	assert.Equal(t, "fatal error", err.Error())
@@ -129,15 +147,15 @@ func TestTransform_Read_EndWithNonContinuableError(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, "fatal error", err.Error())
 	assert.Nil(t, record)
-	raw, err = tfm.CurrentRawRecord()
+	raw, err = tfm.RawRecord()
 	assert.Error(t, err)
 	assert.Equal(t, "fatal error", err.Error())
 	assert.Nil(t, raw)
 }
 
-func TestTransform_CurrentRawRecord_CalledBeforeRead(t *testing.T) {
+func TestTransform_RawRecord_CalledBeforeRead(t *testing.T) {
 	tfm := &transform{ingester: &testIngester{readCalls: []testReadCall{}}}
-	raw, err := tfm.CurrentRawRecord()
+	raw, err := tfm.RawRecord()
 	assert.Error(t, err)
 	assert.Equal(t, "must call Read first", err.Error())
 	assert.Nil(t, raw)
