9293f4bb465677d688fe064748d32e44d0b9f0cc
JF Technology <57098161+jf-tech@users.noreply.github.com>
2022-08-25 16:39:46 +1200

    Adding the new fixed-length2 schema JSON validation.

--- extensions/omniv21/validation/jsonvalidate.go
+++ extensions/omniv21/validation/jsonvalidate.go
@@ -4,3 +4,4 @@ package validation
 //go:generate sh -c "go run ../../../validation/gen/gen.go -json csvFileDeclaration.json -varname JSONSchemaCSVFileDeclaration > ./csvFileDeclaration.go"
 //go:generate sh -c "go run ../../../validation/gen/gen.go -json ediFileDeclaration.json -varname JSONSchemaEDIFileDeclaration > ./ediFileDeclaration.go"
 //go:generate sh -c "go run ../../../validation/gen/gen.go -json fixedlengthFileDeclaration.json -varname JSONSchemaFixedLengthFileDeclaration > ./fixedlengthFileDeclaration.go"
+//go:generate sh -c "go run ../../../validation/gen/gen.go -json fixedlength2FileDeclaration.json -varname JSONSchemaFixedLength2FileDeclaration > ./fixedlength2FileDeclaration.go"
